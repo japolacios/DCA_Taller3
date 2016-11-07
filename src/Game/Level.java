7fc96f9a12b053583cce5838f2500d8c1568595c
Jairo Andres <japolacios@gmail.com>
2016-11-07 17:44:34 -0500

    Game, Meteorito Initial dev

--- src/Game/Level.java
+++ src/Game/Level.java
@@ -7,13 +7,13 @@ import processing.core.PApplet;
 public class Level {
 	
 	//Atributes
-	private int lvl;
+	private int lvl, playerLevel;
 	private PApplet app;
 	private boolean lvlCompleted;
 	
 	//Relations
-	ArrayList<Meteorite> meteorites;
-	ArrayList<Fire> fires;
+	ArrayList<Thread> meteorites;
+	ArrayList<Thread> fires;
 	
 	//Constructor
 	Level(PApplet _app, int _lvl){
@@ -21,9 +21,36 @@ public class Level {
 		lvl = _lvl;
 	}
 	
+	public void recivePlayerStats(int _p, int _c, int _i){
+		playerLevel = _p+_c+_i;
+	}
 	
 	public void setLvlDificulty(){
 		//Sets the amount of meteorites that fall, and how destructive they are
+		if (playerLevel < 100){
+			lvl = 1;
+		}
+		if (playerLevel >= 100 && 500< playerLevel){
+			lvl = 2;
+		}
+		if (playerLevel >= 500 && 700< playerLevel){
+			lvl = 3;
+		}
+		if (playerLevel >= 700 && 1000< playerLevel){
+			lvl = 5;
+		}
+		if (playerLevel >= 1000 ){
+			lvl =(int) ((playerLevel*0.1)/15);
+		}
+	}
+	
+	public void resetLevel(){
+		meteorites = null;
+		fires = null;
+	}
+	
+	public void runLevel(){
+		
 	}
 	
 	public void checkLvlClear(){
--- src/Game/Meteorite.java
+++ src/Game/Meteorite.java
@@ -6,26 +6,38 @@ import processing.core.PShape;
 public class Meteorite extends Thread {
 	
 	//Atributes
-	private int x,y,damage,size,health;
+	private int x,y,damage,lvl,health, yI;
 	private float scale;
 	private PApplet app;
 	private PShape[] shapes;
+	private boolean wait, hit;
 	
 	
 	//Constructor
-	Meteorite(int _x, int _y, int _size, int _health, PApplet _app){
+	Meteorite(int _x, int _y, int _lvl, int _health, PApplet _app){
 		x=_x;
 		y=_y;
-		size = _size;
+		lvl = _lvl;
 		health = _health;
 		app = _app;
+		wait = false;
+		hit = false;
+		yI = _y;
 	}
 	
 	public void run(){
 		while(health > 0){
 			try{
-				//Some Stuff (hit the fire - hit the Floor )
-				sleep(1);
+				if(wait == false){
+					sleep(((int) app.random(0,(lvl*1000))));
+					wait = true;
+				}
+				if (hit == false){
+					fall();
+				}
+				
+				
+				sleep(100);
 			}
 			catch(Exception e){
 				e.printStackTrace();
@@ -34,6 +46,15 @@ public class Meteorite extends Thread {
 		}
 	}
 
+	
+	private void fall(){
+		while(yI+720 <= 720){
+			y++;
+		}
+		if(yI+720 >= 720){
+			hit = true;
+		}
+	}
 	
 	/**************************************
 	 * Getters & Setters
@@ -63,12 +84,12 @@ public class Meteorite extends Thread {
 		this.damage = damage;
 	}
 
-	public int getSize() {
-		return size;
+	public int getLvl() {
+		return lvl;
 	}
 
-	public void setSize(int size) {
-		this.size = size;
+	public void setLvl(int _lvl) {
+		this.lvl = _lvl;
 	}
 
 	public int getHealth() {
